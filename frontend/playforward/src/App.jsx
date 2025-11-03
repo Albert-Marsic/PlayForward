f5920185e623df6ed6edb8f9609b23140a60de3f
MartinVrbovcan <martin.vrbovcan@outlook.com>
2025-11-04 00:32:34 +0100

    Added shadcn UI (+ tailwind)

--- frontend/playforward/src/App.jsx
+++ frontend/playforward/src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button } from '@/components/ui/button'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
 import './App.css'
 
 function App() {
@@ -8,26 +18,32 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Playforward initial!</h1>
+      <NavigationMenu className="fixed top-10 shadow-md z-50">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>O nama</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Vizija</NavigationMenuLink>
+              <NavigationMenuLink>Legionari</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <a href="/prijava">Prijava</a>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <h1>PlayForward stranica!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="flex flex-col items-center justify-center">
+          <Button>Click me</Button>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
