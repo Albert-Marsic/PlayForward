46285a8e24d61de629d773b490aa2abfd23faabe
Hrvoje Juračić <hrvoje.juracic@fer.unizg.hr>
2025-11-12 01:30:05 +0100

    Update App.jsx
    Dodana putanja prema erroru 404 i maknuo sam tada navbar pošto mi nema smisla da se vidi navbar dok je error, na stranici je gumb za povratak na home rutu

--- frontend/playforward/src/App.jsx
+++ frontend/playforward/src/App.jsx
@@ -1,18 +1,24 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Prijava from "./pages/Prijava"
+import Page_404 from "./pages/Page_404"
 import './App.css'
 
 function App() {
+
+  const location = useLocation();
+  const hideNavbar = location.pathname !== "/" && location.pathname !== "/prijava";
+
   return (
     <>
-      <Navbar />
+      {!hideNavbar && <Navbar />}
 
       <main className="pt-16">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/prijava" element={<Prijava />} />
+          <Route path="*" element={<Page_404 />}/>
         </Routes>
       </main>
     </>
